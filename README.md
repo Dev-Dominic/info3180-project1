7a38263fc0965296cd82d0c52f1fe5454c0b65d5
Dominic Henry <dominichenrywork@hotmail.com>
2020-03-07 18:53:59 -0500

    Flask init

---
+++ README.md
@@ -0,0 +1,11 @@
+# INFO3180 Project 1
+
+INFO3180 project 1, showing use of FLASK. 
+
+## How to Run 
+
+```bash
+
+    $ python run.py
+
+```
---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+# FLASK CONFIG
+
+from flask import Flask 
+from flask.ext.sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+# FLASK DATABASE CONFIG
+db = SQLAlchemy(app)
+
+
+from app import views
