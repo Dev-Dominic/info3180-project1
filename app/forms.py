ec32d5a40533354637d7fa1a46fdc77795331f01
Dominic Henry <dominichenrywork@hotmail.com>
2020-04-07 21:57:41 -0500

    Update SignUpForm
    - Import validators module
    
    - Additon of email field and label to create profile template

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
-from wtforms import StringField, SelectField, TextAreaField, FileField, SubmitField
+from wtforms import StringField, SelectField, TextAreaField, FileField, SubmitField, validators
 from wtforms.validators import InputRequired, Email, Length
 
 # User sign-up form
 class SignUpForm(FlaskForm): 
-    firstName = StringField('First Name', validators=[
+    firstname = StringField('First Name', validators=[
             InputRequired(),
             Length(min=3,max=30)
         ])
-    lastName = StringField('Last Name', validators=[
+    lastname = StringField('Last Name', validators=[
             InputRequired(),
             Length(min=3,max=30)
         ])
@@ -24,6 +24,5 @@ class SignUpForm(FlaskForm):
             Length(min=3,max=256)
         ])
     biography = TextAreaField('Biography', validators=[InputRequired()])
-    profilePicture = FileField("Profile Picture", validators=[FileRequired(), FileAllowed(['jpg', 'png']), 'Only jpg and png images allowed!'])
+    profilePicture = FileField("Profile Picture",validators=[FileRequired(), FileAllowed(['jpg', 'png'])])
     submit = SubmitField("Add Profile")
-
