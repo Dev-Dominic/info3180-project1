b5b5fa479e1c3a1edcb721aefb4692918767541f
Dominic Henry <dominichenrywork@hotmail.com>
2020-04-12 18:11:13 -0500

    Fix dbCredentials dictionary

--- config/__init__.py
+++ config/__init__.py
@@ -13,9 +13,9 @@ def getCredentials():
     """
     
     dbCredentials = {
-                os.getenv('DB_NAME')                
-                os.getenv('DB_USER')                
-                os.getenv('DB_PASSWORD')                
+            'DB_NAME' : os.getenv('DB_NAME'),
+            'DB_USER' : os.getenv('DB_USER'),                
+            'DB_PASSWORD' : os.getenv('DB_PASSWORD')
             }
     return dbCredentials
 
